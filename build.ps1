f986ccada0fe5c248ea0dfd82a1a9d0a7be9ecb3
Thomas Broust <tbroust-trepia@ventadis.fr>
2014-09-17 18:35:07 +0200

    Adds the files necessary to the nuget package build

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 Properties {
+	$apikey = $null # call nuget.bat with your Nuget API key as parameter
 	$BIN_PATH = ".\build"
 }
 
@@ -18,13 +19,27 @@ task Cleanup-Binaries {
 # restores nuget packages
 task Restore-Packages {
 	Write-Host "Restoring NuGet packages"
-	& nuget restore
+	& nuget restore CommandLine.sln
 }
 
 # builds the solution
-task Build-Solution {
+task Build-Solution -depends Cleanup-Binaries, Restore-Packages {
 	Write-Host "Building solution"
 	Exec {
 		msbuild "CommandLine.sln" /t:Build /p:Configuration=Release /p:Platform="Any CPU" /p:Warnings=true /v:Normal /nologo /clp:WarningsOnly`;ErrorsOnly`;Summary`;PerformanceSummary
+	}
+}
+
+# generates a Nuget package
+task Generate-Package -depends Build-Solution {
+	if ($apikey -eq $null -or $apikey -eq "") {
+		throw New-Object [System.ArgumentException] "If you want to build a Nuget package, please provide your API key as the batch parameter"
+	} else {
+		Write-Host "Generating a Nuget package"
+		& nuget setApiKey $apikey
+		& nuget Pack ".\nuget\CommandLine.nuspec"
+		Get-ChildItem ".\*.nupkg" | % {
+			& nuget Push $_
+		}
 	}
 }
