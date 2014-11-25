6d84842c25c2df47884b75095199949c5ef1372e
Thomas Broust <thomas.broust@gmail.com>
2014-11-25 16:09:44 +0100

    JustCode automatic cleanup

--- src/CommandLine.Tests/Unit/ParserTests.cs
+++ src/CommandLine.Tests/Unit/ParserTests.cs
@@ -11,6 +11,261 @@ namespace CommandLine.Tests.Unit
     public class ParserTests
     {
         [Fact]
+        public void Long_parameters_names_are_inferred()
+        {
+            // Fixture setup
+            var expectedOptions = new FakeOptions
+                                  {
+                                      StringValue = "strvalue",
+                                      IntSequence = new[]
+                                                    {
+                                                        1,
+                                                        2,
+                                                        3
+                                                    }
+                                  };
+            var sut = new Parser();
+
+            // Exercize system
+            var result = sut.ParseArguments<FakeOptions>(new[]
+                                                         {
+                                                             "--stringvalue",
+                                                             "strvalue",
+                                                             "--intsequence",
+                                                             "1",
+                                                             "2",
+                                                             "3"
+                                                         });
+
+            // Verify outcome
+            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
+            Assert.False(result.Errors.Any());
+            // Teardown
+        }
+
+        [Fact]
+        public void Parse_options()
+        {
+            // Fixture setup
+            var expectedOptions = new FakeOptions
+                                  {
+                                      StringValue = "strvalue",
+                                      IntSequence = new[]
+                                                    {
+                                                        1,
+                                                        2,
+                                                        3
+                                                    }
+                                  };
+            var sut = new Parser();
+
+            // Exercize system
+            var result = sut.ParseArguments<FakeOptions>(new[]
+                                                         {
+                                                             "--stringvalue=strvalue",
+                                                             "-i1",
+                                                             "2",
+                                                             "3"
+                                                         });
+
+            // Verify outcome
+            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
+            Assert.False(result.Errors.Any());
+            // Teardown
+        }
+
+        [Fact]
+        public void Parse_options_with_double_dash()
+        {
+            // Fixture setup
+            var expectedOptions = new FakeOptionsWithValues
+                                  {
+                                      StringValue = "astring",
+                                      LongValue = 20L,
+                                      StringSequence = new[]
+                                                       {
+                                                           "--aaa",
+                                                           "-b",
+                                                           "--ccc"
+                                                       },
+                                      IntValue = 30
+                                  };
+            var sut = new Parser(with => with.EnableDashDash = true);
+
+            // Exercize system
+            var result = sut.ParseArguments<FakeOptionsWithValues>(
+                new[]
+                {
+                    "--stringvalue",
+                    "astring",
+                    "--",
+                    "20",
+                    "--aaa",
+                    "-b",
+                    "--ccc",
+                    "30"
+                });
+
+            // Verify outcome
+            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
+            Assert.False(result.Errors.Any());
+            // Teardown
+        }
+
+        [Fact]
+        public void Parse_options_with_double_dash_in_verbs_scenario()
+        {
+            // Fixture setup
+            var expectedOptions = new AddOptions
+                                  {
+                                      Patch = true,
+                                      FileName = "--strange-fn"
+                                  };
+            var sut = new Parser(with => with.EnableDashDash = true);
+
+            // Exercize system
+            var result = sut.ParseArguments(
+                new[]
+                {
+                    "add",
+                    "-p",
+                    "--",
+                    "--strange-fn"
+                },
+                typeof(AddOptions), typeof(CommitOptions), typeof(CloneOptions));
+
+            // Verify outcome
+            Assert.IsType<AddOptions>(result.Value);
+            result.Value.ShouldHave().AllRuntimeProperties().EqualTo(expectedOptions);
+            Assert.False(result.Errors.Any());
+            // Teardown
+        }
+
+        [Fact]
+        public void Parse_verbs()
+        {
+            // Fixture setup
+            var expectedOptions = new CloneOptions
+                                  {
+                                      Quiet = true,
+                                      Urls = new[]
+                                             {
+                                                 "http://gsscoder.github.com/",
+                                                 "http://yes-to-nooo.github.com/"
+                                             }
+                                  };
+            var sut = new Parser();
+
+            // Exercize system
+            var result = sut.ParseArguments(
+                new[]
+                {
+                    "clone",
+                    "-q",
+                    "http://gsscoder.github.com/",
+                    "http://yes-to-nooo.github.com/"
+                },
+                typeof(AddOptions), typeof(CommitOptions), typeof(CloneOptions));
+
+            // Verify outcome
+            Assert.IsType<CloneOptions>(result.Value);
+            result.Value.ShouldHave().AllRuntimeProperties().EqualTo(expectedOptions);
+            Assert.False(result.Errors.Any());
+            // Teardown
+        }
+
+        [Fact]
+        public void Parse_verbs_using_generic_overload()
+        {
+            // Fixture setup
+            var expectedOptions = new CloneOptions
+                                  {
+                                      Quiet = true,
+                                      Urls = new[]
+                                             {
+                                                 "http://gsscoder.github.com/",
+                                                 "http://yes-to-nooo.github.com/"
+                                             }
+                                  };
+            var sut = new Parser();
+
+            // Exercize system
+            var result = sut.ParseArguments<AddOptions, CommitOptions, CloneOptions>(
+                new[]
+                {
+                    "clone",
+                    "-q",
+                    "http://gsscoder.github.com/",
+                    "http://yes-to-nooo.github.com/"
+                });
+
+            // Verify outcome
+            Assert.IsType<CloneOptions>(result.Value);
+            result.Value.ShouldHave().AllRuntimeProperties().EqualTo(expectedOptions);
+            Assert.False(result.Errors.Any());
+            // Teardown
+        }
+
+        [Fact]
+        public void Same_value_in_two_parameters_are_parsed()
+        {
+            // Fixture setup
+            var expectedOptions = new FakeOptionsWithTwoIntegers
+                                  {
+                                      A = 2,
+                                      B = 2
+                                  };
+            var sut = new Parser();
+
+            // Exercize system
+            var result = sut.ParseArguments<FakeOptionsWithTwoIntegers>(new[]
+                                                                        {
+                                                                            "-a",
+                                                                            "2",
+                                                                            "-b",
+                                                                            "2"
+                                                                        });
+
+            // Verify outcome
+            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
+            Assert.False(result.Errors.Any());
+            // Teardown
+        }
+
+        [Fact]
+        public void Short_Parameters_names_are_used()
+        {
+            // Fixture setup
+            var expectedOptions = new FakeOptions
+                                  {
+                                      IntSequence = new[]
+                                                    {
+                                                        1,
+                                                        2,
+                                                        3
+                                                    },
+                                      BoolValue = true
+                                  };
+            var sut = new Parser();
+
+            // Exercize system
+            var result = sut.ParseArguments<FakeOptions>(new[]
+                                                         {
+                                                             "-x",
+                                                             "true",
+                                                             "-i",
+                                                             "1",
+                                                             "2",
+                                                             "3"
+                                                         });
+
+            // Verify outcome
+            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
+            Assert.False(result.Errors.Any());
+            // Teardown
+        }
+
+        [Fact]
         public void When_HelpWriter_is_set_help_screen_is_generated()
         {
             // Fixture setup
@@ -55,176 +310,6 @@ namespace CommandLine.Tests.Unit
             // Verify outcome
             var text = writer.ToString();
             Assert.True(text.Length > 0);
-            // Teardown
-        }
-
-        [Fact]
-        public void Parse_options()
-        {
-            // Fixture setup
-            var expectedOptions = new FakeOptions
-                {
-                    StringValue = "strvalue", IntSequence = new[] { 1, 2, 3 }
-                };
-            var sut = new Parser();
-
-            // Exercize system
-            var result = sut.ParseArguments<FakeOptions>(new[] { "--stringvalue=strvalue", "-i1", "2", "3" });
-
-            // Verify outcome
-            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
-            Assert.False(result.Errors.Any());
-            // Teardown
-        }
-
-        [Fact]
-        public void Parameters_names_are_inferred()
-        {
-            // Fixture setup
-            var expectedOptions = new FakeOptions
-            {
-                StringValue = "strvalue",
-                IntSequence = new [] { 1, 2, 3 }
-            };
-            var sut = new Parser();
-
-            // Exercize system
-            var result = sut.ParseArguments<FakeOptions>(new[] { "--stringvalue", "strvalue", "--intsequence", "1", "2", "3" });
-
-            // Verify outcome
-            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
-            Assert.False(result.Errors.Any());
-            // Teardown
-        }
-
-        [Fact]
-        public void Same_value_in_two_parameters_are_parsed()
-        {
-            // Fixture setup
-            var expectedOptions = new FakeOptionsWithTwoIntegers
-            {
-                A = 2,
-                B = 2
-            };
-            var sut = new Parser();
-
-            // Exercize system
-            var result = sut.ParseArguments<FakeOptionsWithTwoIntegers>(new[] { "-a", "2", "-b", "2" });
-
-            // Verify outcome
-            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
-            Assert.False(result.Errors.Any());
-            // Teardown
-        }
-
-        [Fact]
-        public void Short_Parameters_names_are_used()
-        {
-            // Fixture setup
-            var expectedOptions = new FakeOptions
-            {
-                IntSequence = new[] { 1, 2, 3 },
-                BoolValue = true
-            };
-            var sut = new Parser();
-
-            // Exercize system
-            var result = sut.ParseArguments<FakeOptions>(new[] { "-x", "true", "-i1", "2", "3" });
-
-            // Verify outcome
-            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
-            Assert.False(result.Errors.Any());
-            // Teardown
-        }
-
-        [Fact]
-        public void Parse_options_with_double_dash()
-        {
-            // Fixture setup
-            var expectedOptions = new FakeOptionsWithValues
-                {
-                    StringValue = "astring",
-                    LongValue = 20L,
-                    StringSequence = new[] { "--aaa", "-b", "--ccc" },
-                    IntValue = 30
-                };
-            var sut = new Parser(with => with.EnableDashDash = true);
-
-            // Exercize system
-            var result = sut.ParseArguments<FakeOptionsWithValues>(
-                new[] { "--stringvalue", "astring", "--", "20", "--aaa", "-b", "--ccc", "30" });
-
-            // Verify outcome
-            result.Value.ShouldHave().AllProperties().EqualTo(expectedOptions);
-            Assert.False(result.Errors.Any());
-            // Teardown
-        }
-
-        [Fact]
-        public void Parse_options_with_double_dash_in_verbs_scenario()
-        {
-            // Fixture setup
-            var expectedOptions = new AddOptions
-                {
-                    Patch = true,
-                    FileName = "--strange-fn"
-                };
-            var sut = new Parser(with => with.EnableDashDash = true);
-
-            // Exercize system
-            var result = sut.ParseArguments(
-                new[] { "add", "-p", "--", "--strange-fn" },
-                typeof(AddOptions), typeof(CommitOptions), typeof(CloneOptions));
-
-            // Verify outcome
-            Assert.IsType<AddOptions>(result.Value);
-            result.Value.ShouldHave().AllRuntimeProperties().EqualTo(expectedOptions);
-            Assert.False(result.Errors.Any());
-            // Teardown
-        }
-
-        [Fact]
-        public void Parse_verbs()
-        {
-            // Fixture setup
-            var expectedOptions = new CloneOptions
-                {
-                    Quiet = true,
-                    Urls = new[] { "http://gsscoder.github.com/", "http://yes-to-nooo.github.com/" }
-                };
-            var sut = new Parser();
-
-            // Exercize system
-            var result = sut.ParseArguments(
-                new[] { "clone", "-q", "http://gsscoder.github.com/", "http://yes-to-nooo.github.com/" },
-                typeof(AddOptions), typeof(CommitOptions), typeof(CloneOptions));
-
-            // Verify outcome
-            Assert.IsType<CloneOptions>(result.Value);
-            result.Value.ShouldHave().AllRuntimeProperties().EqualTo(expectedOptions);
-            Assert.False(result.Errors.Any());
-            // Teardown
-        }
-
-        [Fact]
-        public void Parse_verbs_using_generic_overload()
-        {
-            // Fixture setup
-            var expectedOptions = new CloneOptions
-            {
-                Quiet = true,
-                Urls = new[] { "http://gsscoder.github.com/", "http://yes-to-nooo.github.com/" }
-            };
-            var sut = new Parser();
-
-            // Exercize system
-            var result = sut.ParseArguments<AddOptions, CommitOptions, CloneOptions>(
-                new[] { "clone", "-q", "http://gsscoder.github.com/", "http://yes-to-nooo.github.com/" });
-
-            // Verify outcome
-            Assert.IsType<CloneOptions>(result.Value);
-            result.Value.ShouldHave().AllRuntimeProperties().EqualTo(expectedOptions);
-            Assert.False(result.Errors.Any());
             // Teardown
         }
     }
